e23533f578bb4811f8164d6fd1f2f4fa9bf77762
sam <sammtcbn@gmail.com>
2022-09-08 23:17:20 +0800

    script to demo fastdds

---
+++ fastdds_demo_sub.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+# refer to https://github.com/ros2/rmw_fastrtps
+source /opt/ros/foxy/setup.bash
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+RMW_IMPLEMENTATION=rmw_fastrtps_cpp ros2 run demo_nodes_cpp listener
