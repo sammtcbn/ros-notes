226f9211aaa1db60a3cc4476ed8779f76a7138c5
sam <sammtcbn@gmail.com>
2022-09-13 01:41:17 +0800

    script to install MoveIt

---
+++ moveit_install.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+# MoveIt
+# refer to https://moveit.ros.org/install-moveit2/binary/
+
+rosdist=foxy
+
+function failed()
+{
+    echo "$*" >&2
+    exit 1
+}
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+sudo apt -y install ros-${rosdist}-moveit || failed "apt install ros-${rosdist}-moveit fail"
+
+# refer to https://wiki.ros.org/rqt/UserGuide/Install/Groovy
+sudo apt -y install ros-${rosdist}-rqt-moveit || failed "apt install ros-${rosdist}-rqt-moveit fail"
