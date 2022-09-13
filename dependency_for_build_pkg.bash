0df88c11e2f11863a0389ae05104c598e5e2c67b
sam <sammtcbn@gmail.com>
2022-09-14 00:18:24 +0800

    update script for install dependency for develop ros

--- dependency_for_build_pkg.bash
+++ dependency_for_build_pkg.bash
@@ -9,8 +9,19 @@ function failed()
     exit 1
 }
 
-sudo apt -y install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential || failed "fail to install dependencies for build ROS package"
+sudo apt -y install git || failed "fail to install git"
+sudo apt -y install python3-argcomplete || failed "fail to install python3-argcomplete"
+sudo apt -y install python3-pip || failed "fail to install python3-pip"
+sudo apt -y install build-essential || failed "fail to install build-essential"
+sudo apt -y install python3-vcstool || failed "fail to install python3-vcstool"
+sudo apt -y install python3-rosdep || failed "failed to install python3-rosdep"
+sudo apt -y install python3-rosinstall || failed "failed to install python3-rosinstall"
+sudo apt -y install python3-rosinstall-generator || failed "fail to install python3-rosinstall-generator"
+sudo apt -y install python3-wstool || failed "fail to install python3-wstool"
+sudo apt -y install python3-colcon-common-extensions || failed "fail to install python3-colcon-common-extensions"
 
 # Initialize rosdep
-sudo rosdep init
-rosdep update
+sudo rosdep init || failed "fail to rosdep init"
+rosdep update || failed "fail to rosdep update"
+
+echo "done"
