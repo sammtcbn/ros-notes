df7efb3743fba0ed3a90de93705e0f2245421500
sammtcbn <sammtcbn@gmail.com>
2022-09-13 10:49:23 +0800

    remove ros setup line from .bashrc while uninstall

--- ros2-uninstall.bash
+++ ros2-uninstall.bash
@@ -17,3 +17,6 @@ sudo rm -f /etc/apt/sources.list.d/ros2.list || exit "delete ros2 source list fa
 sudo apt -y update || failed "apt update fail"
 sudo apt -y autoremove || failed "apt autoremove fail"
 sudo apt -y upgrade || failed "apt upgrade fail"
+
+# remove "source /opt/ros/foxy/setup.bash" line from ~/.bashrc
+sed -i "/source \/opt\/ros\/${ROS_DISTRO}\/setup.bash/d" ~/.bashrc
